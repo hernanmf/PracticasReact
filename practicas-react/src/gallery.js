1e1c39e4e9da75c8d1234611c2168672972c1107
Hernan Martinez <hernanmartinezf@gmail.com>
2023-02-22 14:44:10 -0300

    Desafio 1 resuelto

--- practicas-react/src/gallery.js
+++ practicas-react/src/gallery.js
@@ -4,13 +4,26 @@ import { sculptureList } from "./data.js";
 export function Gallery() {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(0);
+  const [btnPrevActivo, setBtnPrevActivo] = useState(false);
+  const [btnNextActivo, setBtnNextActivo] = useState(
+    sculptureList.length === 1 ? false : true
+  );
 
   function handleNextClick() {
+    setBtnPrevActivo(true);
     setIndex(index + 1);
+    if (index + 1 === sculptureList.length - 1) {
+      /* se pone lenght -1 porque cuando se hace el click en el 11 se tiene q inactivar el boton siguiente */
+      setBtnNextActivo(false);
+    }
   }
 
   function handlePrevClick() {
+    setBtnNextActivo(true);
     setIndex(index - 1);
+    if (index - 0 === 1) {
+      setBtnPrevActivo(false);
+    }
   }
 
   function handleMoreClick() {
@@ -20,13 +33,11 @@ export function Gallery() {
   let sculpture = sculptureList[index];
   return (
     <section>
-      <button onClick={handlePrevClick} disabled="tru">
-        {" "}
-        Anterior{" "}
+      <button onClick={handlePrevClick} disabled={!btnPrevActivo}>
+        Anterior
       </button>
-      <button onClick={handleNextClick} disabled>
-        {" "}
-        Siguiente{" "}
+      <button onClick={handleNextClick} disabled={!btnNextActivo}>
+        Siguiente
       </button>
       <h2>
         <i>{sculpture.name}</i> por {sculpture.artist}
