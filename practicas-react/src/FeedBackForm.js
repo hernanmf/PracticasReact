9b2eff3e857529270225fb350f91b3033a416f14
Hernan Martinez <hernanmartinezf@gmail.com>
2023-03-02 14:31:56 -0300

    Desafio 3 realizado

--- practicas-react/src/FeedBackForm.js
+++ practicas-react/src/FeedBackForm.js
@@ -2,29 +2,28 @@ import { useState } from "react";
 
 export function FeedbackForm() {
   const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState("");
 
-  if (isSent) {
-    return <h1>¡Gracias!</h1>;
-  } else {
-    // eslint-disable-next-line
-    const [message, setMessage] = useState("");
-    return (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          alert(`Sending: "${message}"`);
-          setIsSent(true);
-          alert("¡Gracias!");
-        }}
-      >
-        <textarea
-          placeholder="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <br />
-        <button type="submit">Enviar</button>
-      </form>
-    );
-  }
+  const handleSent = (e) => {
+    if (!isSent) {
+      e.preventDefault();
+      alert(`Sending: "${message}"`);
+      setIsSent(true);
+      alert("¡Gracias!");
+    } else {
+      alert("Su mensaje ya fue enviado. ¡Gracias!");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSent}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <br />
+      <button type="submit">Enviar</button>
+    </form>
+  );
 }
